5e1b2f772b20d8c9ec874caf0d585a30b3e50a3d
unknown <mohammadwawan.24@gmail.com>
2026-03-25 11:53:15 +0700

    fixing error table order has no column named submittedRating

---
+++ src/drizzle/0001_vengeful_wolverine.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `menu` ADD `nutrition` text;--> statement-breakpoint
+ALTER TABLE `order` ADD `roomNumber` text;--> statement-breakpoint
+ALTER TABLE `order` ADD `reviewText` text;--> statement-breakpoint
+ALTER TABLE `order` ADD `submittedRating` integer;--> statement-breakpoint
+ALTER TABLE `order` ADD `updatedAt` integer;--> statement-breakpoint
+ALTER TABLE `order` ADD `validatedByName` text;--> statement-breakpoint
+ALTER TABLE `order` ADD `preparingByName` text;--> statement-breakpoint
+ALTER TABLE `order` ADD `readyByName` text;--> statement-breakpoint
+ALTER TABLE `order` ADD `deliveringByName` text;--> statement-breakpoint
+ALTER TABLE `order` ADD `deliveredByName` text;
