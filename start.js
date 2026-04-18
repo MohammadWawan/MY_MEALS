63c3bec22eeb0972f36095ebef9a7952ab5cf18f
unknown <mohammadwawan.24@gmail.com>
2026-04-19 00:54:00 +0700

    remove standalone

--- start.js
+++
@@ -1,4 +0,0 @@
-process.env.HOSTNAME = '10.48.3.2';
-process.env.PORT = '3003';
-console.log(`Starting standalone server at http://${process.env.HOSTNAME}:${process.env.PORT}`);
-require('./.next/standalone/server.js');
